48f69b0c75f08e3b6015df82f0519e6a021eddf4
Nitish <nitish.gupta.unsw@gmail.com>
2017-07-01 11:12:43 +0530

    second" " ;

--- server.js
+++ server.js
@@ -2,3 +2,19 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/testDB');
 
 var newCar =  require('./models/car');
+
+var Car = new newCar({
+  name: "Mercedes Benz",
+  type: "E-Class",
+  body: "Silver",
+  price: [{body: "40 Lakhs", date: "12/12/12"}],
+  fuel: "Petrol"
+});
+
+Car.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Car Saved.');
+  }
+});
