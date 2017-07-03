3e5d49371490a318cb285396cbff386d28ae64fc
Nitish <nitish.gupta.unsw@gmail.com>
2017-07-03 11:35:46 +0530

    Aggregate

--- runner.js
+++ runner.js
@@ -12,23 +12,35 @@ var restaurant = require('./models/restaurant');
 //    }
 // });
 
-restaurant.findOne({'name':'Kosher Island'},function(err,data){
-	if(err){
-		console.log(err)
-	}
-	console.log(data)
-})
+// restaurant.findOne({'name':'Kosher Island'},function(err,data){
+// 	if(err){
+// 		console.log(err)
+// 	}
+// 	console.log(data)
+// })
 
-restaurant.findOneAndUpdate({'name':'Kosher Island'}, {$set:{borough:'NEW UPDATED'}},function(err,data){
-	if(err){
-		console.log(err)
-	}
-	console.log(data)
-})
+// restaurant.findOneAndUpdate({'name':'Kosher Island'}, {$set:{borough:'NEW UPDATED'}},function(err,data){
+// 	if(err){
+// 		console.log(err)
+// 	}
+// 	console.log(data)
+// })
 
-restaurant.findOne({'name':'Kosher Island'},function(err,data){
-	if(err){
-		console.log(err)
-	}
-	console.log('Updated',data)
-})
+// restaurant.findOne({'name':'Kosher Island'},function(err,data){
+// 	if(err){
+// 		console.log(err)
+// 	}
+// 	console.log('Updated',data)
+// })
+
+restaurant.aggregate(
+    [
+        { $group: { "_id": "$borough", "count": { $sum: 1 } } }
+    ],
+    function(err, data) {
+        if (err) {
+            console.log(err)
+        }
+        console.log(data)
+    }
+)
